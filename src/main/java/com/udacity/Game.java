29effddaaa9997b7cb42e66cd788e7875edec52d
ynyjcxyz <83985909+ynyjcxyz@users.noreply.github.com>
2021-05-10 14:27:53 -0700

    Initial commit

---
+++ src/main/java/com/udacity/Game.java
@@ -0,0 +1,223 @@
+package com.udacity; //打包相关
+
+import java.util.Arrays;  //导入某个数组类
+
+/**
+ * Created by udacity 2016
+ * The Main class containing game logic and backend 2D array
+ */
+public class Game {
+
+    private char turn; // who's turn is it, 'x' or 'o' ? x always starts  //声明字符类型私有变量
+    private boolean twoPlayer; // true if this is a 2 player game, false if AI playing  //声明布尔型私有变量
+    private char [][] grid; // a 2D array of chars representing the game grid  //声明整型私有变量
+    private int freeSpots; // counts the number of empty spots remaining on the board (starts from 9  and counts down)  //声明整型私有变量
+    private static GameUI gui;  //作用不明
+
+    /**
+     * Create a new single player game
+     *
+     */
+    public Game() {
+        newGame(false);
+    }  //声明公共函数
+
+    /**
+     * Create a new game by clearing the 2D grid and restarting the freeSpots counter and setting the turn to x
+     * @param twoPlayer: true if this is a 2 player game, false if playing against the computer
+     */
+    public void newGame(boolean twoPlayer){  //开启一个新游戏
+        //sets a game to one or two player
+        this.twoPlayer = twoPlayer;  //布尔变量赋值给指定目标
+
+        // initialize all chars in 3x3 game grid to '-'
+        grid = new char[3][3]; //开辟一个新的3X3数组
+        //fill all empty slots with -
+        for (int i=0; i<3; i++){   //使用嵌套的for循环来给3X3数组中的每一个位置赋值'-',得到3X3的'-'
+            for (int j=0; j<3; j++){
+                grid[i][j] = '-';
+            }
+        }
+        //start with 9 free spots and decrement by one every time a spot is taken
+        freeSpots = 9; //私有变量赋值9
+        //x always starts
+        turn = 'x';  //总是从X开始
+    }
+
+
+    /**
+     * Gets the char value at that particular position in the grid array
+     * @param i the x index of the 2D array grid
+     * @param j the y index of the 2D array grid
+     * @return the char value at the position (i,j):
+     *          'x' if x has played here
+     *          'o' if o has played here
+     *          '-' if no one has played here
+     *          '!' if i or j is out of bounds
+     */
+    public char gridAt(int i, int j){  //i,j超越3x3边界的情形,返回'!'(i,j从0到2)
+        if(i>=3||j>=3||i<0||j<0)
+            return '!';
+        return grid[i][j];
+    }
+
+    /**
+     * Places current player's char at position (i,j)
+     * Uses the variable turn to decide what char to use
+     * @param i the x index of the 2D array grid
+     * @param j the y index of the 2D array grid
+     * @return boolean: true if play was successful, false if invalid play
+     */
+    public boolean playAt(int i, int j){  //
+        //check for index boundries
+        if(i>=3||j>=3||i<0||j<0)
+            return false;
+        //check if this position is available
+        if(grid[i][j] != '-'){
+            return false; //bail out if not available
+        }
+        //update grid with new play based on who's turn it is
+        grid[i][j] = turn;
+        //update free spots
+        freeSpots--;
+        return true;
+    }
+
+
+    /**
+     * Override
+     * @return string format for 2D array values
+     */
+    public String toString(){
+        return Arrays.deepToString(this.grid);
+    }
+
+    /**
+     * Performs the winner chack and displayes a message if game is over
+     * @return true if game is over to start a new game
+     */
+    public boolean doChecks() {
+        //check if there's a winner or tie ?
+        String winnerMessage = checkGameWinner(grid);
+        if (!winnerMessage.equals("None")) {
+            gui.gameOver(winnerMessage);
+            newGame(false);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Allows computer to play in a single player game or switch turns for 2 player game
+     */
+    public void nextTurn(){
+        //check if single player game, then let computer play turn
+        if(!twoPlayer){
+            if(freeSpots == 0){
+                return ; //bail out if no more free spots
+            }
+            int ai_i, ai_j;
+            do {
+                //randomly pick a position (ai_i,ai_j)
+                ai_i = (int) (Math.random() * 3);
+                ai_j = (int) (Math.random() * 3);
+            }while(grid[ai_i][ai_j] != '-'); //keep trying if this spot was taken
+            //update grid with new play, computer is always o
+            grid[ai_i][ai_j] = 'o';
+            //update free spots
+            freeSpots--;
+        }
+        else{
+            //change turns
+            if(turn == 'x'){
+                turn = 'o';
+            }
+            else{
+                turn = 'x';
+            }
+        }
+        return;
+    }
+
+
+    /**
+     * Checks if the game has ended either because a player has won, or if the game has ended as a tie.
+     * If game hasn't ended the return string has to be "None",
+     * If the game ends as tie, the return string has to be "Tie",
+     * If the game ends because there's a winner, it should return "X wins" or "O wins" accordingly
+     * @param grid 2D array of characters representing the game board
+     * @return String indicating the outcome of the game: "X wins" or "O wins" or "Tie" or "None"
+     */
+    public String checkGameWinner(char [][]grid){
+        String result = "None";
+        //Student code goes here ...
+        Switch(grid[][]){
+            case1: if(grid[0][0]==grid[1][0]==grid[2][0]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[0][0]==grid[1][0]==grid[2][0]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case2:if(grid[0][1]==grid[1][1]==grid[2][1]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[0][1]==grid[1][1]==grid[2][1]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case3:if(grid[0][2]==grid[1][2]==grid[2][2]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[0][2]==grid[1][2]==grid[2][2]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case4:if(grid[0][0]==grid[0][1]==grid[0][2]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[0][0]==grid[0][1]==grid[0][2]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case5:if(grid[1][0]==grid[1][1]==grid[1][2]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[1][0]==grid[1][1]==grid[1][2]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case6:if(grid[2][0]==grid[2][1]==grid[2][2]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[2][0]==grid[2][1]==grid[2][2]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case7:if(grid[0][0]==grid[1][1]==grid[2][2]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[0][0]==grid[1][1]==grid[2][2]=='o'){
+                    result="o wins";
+                    }
+                    break;
+            case8:if(grid[2][0]==grid[1][1]==grid[0][2]=='x'){
+                    result="x wins";
+                    }
+                    else if(grid[2][0]==grid[1][1]==grid[0][2]=='o'){
+                    result="o wins";
+                    }
+                    break;
+                    
+        return result;
+
+    /**
+     * Main function
+     * @param args command line arguments
+     */
+    public static void main(String args[]){
+        Game game = new Game();
+        gui = new GameUI(game);
+    }
+
+}
